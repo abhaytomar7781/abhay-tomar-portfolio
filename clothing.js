c92f244b10e85e587f272eec9df0cb25e2f42e63
abhaytomar7781 <abhaytomar7781@gmail.com>
2025-12-18 12:35:20 +0530

    Add files via upload

---
+++ clothing.js
@@ -0,0 +1,11 @@
+let cartCount = 0;
+
+    function addToCart(product) {
+      cartCount++;
+      document.getElementById('count').innerText = cartCount;
+      alert(product + ' added to cart!');
+    }
+
+    function scrollToProducts() {
+      document.getElementById('products').scrollIntoView({ behavior: 'smooth' });
+    }
